0edc74c3c6a879d61bb44033569cf5d6c5acfbe7
Arthur Iliescu <voidcsgo.yt@gmail.com>
2025-10-24 22:21:59 -0400

    added one visualization and skeelton for another as well as page swapping func

--- js/visualization2/calendar.js
+++ js/visualization2/calendar.js
@@ -1,9 +1,5 @@
 /* Calendar Visualization js file for component class */
 
-
-
-  /* Class to load in all TTC Subway Station Data into a map visualization */
-
 class CalendarMap {
 
 
@@ -12,39 +8,200 @@ class CalendarMap {
         this.parentElement = parentElement;
         this.data = data;
         this.flightsPerDay = {};
+        this.calendarPadding = 30;
 
     }
 
 
-/* method for loading all flights and their data
-NOTE: Since there are two dates to consider, we take the first date as the flight date for flights that
-Cross timezone regions or technically cross the time into the next date*/
-
-  loadFlights() {
+    loadFlights() {
 
         console.log("loadFlights");
         var vis = this;
+        console.log(vis.data);
 
-        // Group flights by day
-        vis.data.forEach(flight => {
-            //take only the date portion of the flight data from firstseen.
+        //group flights by day
+        vis.data.data.forEach(flight => {
+            
 
-            const day = flight.firstseen_filtered.toISOString().split("T")[0];
+            var day = flight.day.toISOString().split("T")[0];
 
-            //if this is the first flight for this day initialize array containing flights
+            
             if (!vis.flightsPerDay[day]) {
                 vis.flightsPerDay[day] = [];
             }
 
-            vis.flightsPerDay[day].push(flight);
+            vis.flightsPerDay[day].push(flight.total_flights);
 
         });
 
-
-
         console.log("Flights per day:", vis.flightsPerDay);
+        
+        vis.initVis();
     }
 
 
+    initVis(){
 
-  }
+        let vis = this;
+
+		vis.margin = {top: 40, right: 40, bottom: 60, left: 40};
+
+		vis.width = document.getElementById(vis.parentElement).getBoundingClientRect().width - vis.margin.left - vis.margin.right;
+		vis.height = document.getElementById(vis.parentElement).getBoundingClientRect().height - vis.margin.top - vis.margin.bottom;
+
+		//svg drawing area
+		vis.svg = d3.select("#" + vis.parentElement).append("svg")
+			.attr("width", vis.width + vis.margin.left + vis.margin.right)
+			.attr("height", vis.height + vis.margin.top + vis.margin.bottom)
+			.append("g")
+			.attr("transform", "translate(" + vis.margin.left + "," + vis.margin.top + ")");
+
+
+        //overlay with path clipping
+		vis.svg.append("defs").append("clipPath")
+			.attr("id", "clip")
+
+			.append("rect")
+			.attr("width", vis.width)
+			.attr("height", vis.height);
+        
+        
+        //console.log("vis.data.data:", vis.data.data);
+
+
+
+        //scales and axes stuff
+        vis.x = d3.scaleTime()
+            .range([vis.calendarPadding, vis.width - vis.calendarPadding])
+            .domain([d3.min(vis.data.data, d => d.day), d3.max(vis.data.data, d => d.day)]);
+
+        vis.y = d3.scaleLinear()
+            .range([vis.height - vis.calendarPadding, vis.calendarPadding])
+            .domain([0, d3.max(vis.data.data, d => d.total_flights)]);
+
+        vis.xAxis = d3.axisBottom()
+            .scale(vis.x)
+            .tickFormat(d3.timeFormat("%b %d"));    
+
+        vis.yAxis = d3.axisLeft()
+            .scale(vis.y);
+
+        vis.svg.append("g")
+            .attr("class", "x-axis axis")
+            .attr("transform", "translate(0," + (vis.height - vis.calendarPadding) + ")")
+            .call(vis.xAxis);
+
+        vis.svg.append("g")
+            .attr("class", "y-axis axis")
+            .attr("transform", "translate(" + (vis.calendarPadding) + ",0)")
+            .call(vis.yAxis);
+   
+        vis.svg.append("text")
+            .attr("class", "axis-label")
+            .attr("text-anchor", "middle")
+            .attr("x", vis.width / 2)
+            .attr("y", vis.height + vis.calendarPadding)
+            .style("font-size", "14px")
+            .style("fill", "#ffffff")
+            .style("font-weight", "bold")
+            .text("Date (Month and Day)");
+     
+        vis.svg.append("text")
+            .attr("class", "axis-label")
+            .attr("text-anchor", "middle")
+            .attr("x", 0 + vis.calendarPadding)
+            .attr("y", 0)
+            .style("font-size", "14px")
+            .style("fill", "#ffffff")
+            .style("font-weight", "bold")
+            .text("Total Flights");
+
+
+
+
+        //initalizing the tooltip part
+        var tooltip = d3.select("body").append("div")
+            .attr("class", "calendar-tooltip")
+            .style("position", "absolute")
+            .style("padding", "10px")
+            .style("background", "rgba(0, 0, 0, 0.8)")
+            .style("color", "white")
+            .style("border-radius", "8px")
+            .style("font-size", "14px")
+            .style("pointer-events", "none")
+            .style("opacity", 0);
+
+
+        //path between each point handled here
+        vis.svg.append("path")
+            .datum(vis.data.data)
+            .attr("class", "data-line")
+            .attr("fill", "none")
+            .attr("stroke", "#307c32ff")
+            .attr("stroke-width", 2)
+            .attr("opacity", 1)
+            .attr("d", function(d) {
+                
+                var line = d3.line()
+                    .x(d => vis.x(d.day))
+                    .y(d => vis.y(d.total_flights))
+                    .curve(d3.curveLinear);
+
+                return line(d);
+
+            });
+
+
+        //Adding of data points for each day
+        vis.svg.selectAll(".day-data")
+            .data(vis.data.data)
+            .enter()
+            .append("circle")
+            .attr("class", "day-data")
+            .attr("cx", d => vis.x(d.day))
+            .attr("cy", d => vis.y(d.total_flights))
+            .attr("r", 8)
+            .attr("fill", "#38e03eff")
+            .attr("stroke", "#000000ff")
+            .attr("stroke-width", 2)
+
+            //my hover events
+            .on("mouseover", function(event, d) {
+                
+                d3.select(this)
+                    .transition()
+                    .duration(200)
+                    .attr("r", 20)
+                    .attr("fill", "#37a2ffff");
+
+                
+                tooltip.style("opacity", 1)
+                    .html(`<strong>Date:</strong> ${d.day.toDateString()}<br/>
+                    <strong>Flights:</strong> ${d.total_flights}`)
+                    .style("left", (event.pageX + 20) + "px")
+                    .style("top", (event.pageY - 20) + "px");
+                
+            })
+
+            //handling after the hover events
+            .on("mouseout", function() {
+                
+                d3.select(this)
+                    .transition()
+                    .duration(200)
+                    .attr("r", 8)
+                    .attr("fill", "#38e03eff");
+                
+               
+                tooltip.style("opacity", 0);
+
+
+            });
+
+
+
+
+    }
+
+
+}
--- js/visualization2/main.js
+++ js/visualization2/main.js
@@ -1,60 +1,116 @@
-/* main file for loading data on visualization 2    */
-
-
-/* file description copied off of data set website https://zenodo.org/records/7923702 */
-
-/* One file per month is provided as a csv file with the following features:
-
-callsign: the identifier of the flight displayed on ATC screens (usually the first three letters are reserved for an airline: AFR for Air France, DLH for Lufthansa, etc.)
-number: the commercial number of the flight, when available (the matching with the callsign comes from public open API); this field may not be very reliable;
-icao24: the transponder unique identification number;
-registration: the aircraft tail number (when available);
-typecode: the aircraft model type (when available);
-origin: a four letter code for the origin airport of the flight (when available);
-destination: a four letter code for the destination airport of the flight (when available);
-firstseen: the UTC timestamp of the first message received by the OpenSky Network;
-lastseen: the UTC timestamp of the last message received by the OpenSky Network;
-day: the UTC day of the last message received by the OpenSky Network;
-latitude_1, longitude_1, altitude_1: the first detected position of the aircraft;
-latitude_2, longitude_2, altitude_2: the last detected position of the aircraft. 
-
-first seen last seen separated into this format: 2019-01-31 00:14:53+00:00
-
-Need to splice space to get date and time.
-
-*/
-
 let calendarMap;
 
+let monthlyDataVis2 = [];
+
 //start application by loading the data
-loadData();
+loadDataVis2();
 
 
-function loadData() {
-  //load the TTC CSV
-  d3.csv("data/flightlist_february_2019.csv", row => {
 
-	//separating the date from the time from string to actualy date time values.
-    firstSeenParts = row.firstseen.split(" ");
-    row.firstseen_filtered = new Date(firstSeenParts[0] + "T" + firstSeenParts[1]);
-    lastSeenParts = row.lastseen.split(" ");
-    row.lastseen_filtered = new Date(lastSeenParts[0] + "T" + lastSeenParts[1]);
-    return row;
+function loadDataVis2() {
+  //load each months data
+  var promise1 = d3.csv("data/2019_01_dailytotals.csv", row => {
 
-  }).then(data => {
+    //fixing utc to est time zone bug
+    var dateParts = row.day.split('-');
+    row.day = new Date(+dateParts[0], +dateParts[1] - 1, +dateParts[2]);
+    row.total_flights = +row.unique_icao24_count;
 
-    //filter data to only include flights that have lat, long, origin, dest, first and last seen
-    const flights = data.filter(d => {
-      return d.latitude_1 && d.longitude_1 && d.latitude_2 && d.longitude_2 && d.origin && d.destination
-      && d.firstseen && d.lastseen;
-    });
+    return row = {
 
-    //log the first 20 entries of flights to verify
-    console.log(flights.slice(0, 20));
-    //Initialize the Calendar Map with flight data
-    calendarMap = new CalendarMap("visualization-1", flights);
+      day: row.day,
+      total_flights: row.total_flights
+      
+    };
+
+  });
+
+  //load each months data
+  var promise2 = d3.csv("data/2020_04_dailytotals.csv", row => {
+
+    //fixing utc to est time zone bug
+    var dateParts = row.day.split('-');
+    row.day = new Date(+dateParts[0], +dateParts[1] - 1, +dateParts[2]);
+    row.total_flights = +row.unique_icao24_count;
+
+    return row = {
+
+      day: row.day,
+      total_flights: row.total_flights
+      
+    };
+
+  });
+
+  //load each months data
+  var promise3 = d3.csv("data/2022_12_dailytotals.csv", row => {
+
+    //fixing utc to est time zone bug
+    var dateParts = row.day.split('-');
+    row.day = new Date(+dateParts[0], +dateParts[1] - 1, +dateParts[2]);
+    row.total_flights = +row.unique_icao24_count;
+
+    return row = {
+
+      day: row.day,
+      total_flights: row.total_flights
+      
+    };
+
+  });
+
+  Promise.all([promise1, promise2, promise3]).then((data1) => {
+
+    monthlyDataVis2.push({ month: "2019_01", data: data1[0], label: "January 2019" });
+    monthlyDataVis2.push({ month: "2020_04", data: data1[1], label: "April 2020" });
+    monthlyDataVis2.push({ month: "2022_12", data: data1[2], label: "December 2022" });
+    console.log("All monthly data loaded for Vis2:", monthlyDataVis2);
+
+    createMonthSelectorVis2();
+
+    //Initialize the Calendar Map with January 2019 data
+    calendarMap = new CalendarMap("visualization-2", monthlyDataVis2[0]);
     calendarMap.loadFlights();
 
   });
 
 }
+
+
+function createMonthSelectorVis2() {
+
+       
+    var buttonContainer = d3.select("#visualization-2").append("div")
+        .attr("class", "month-selector")
+        .style("text-align", "center");
+    
+    var buttons = buttonContainer.selectAll(".month-button")
+        .data(monthlyDataVis2)
+        .enter()
+        .append("button")
+        .attr("class", "month-button")
+        .style("margin", "5px")
+        .style("padding", "10px")
+        .style("border", "2px solid #000000ff")
+        .style("border-radius", "10px")
+        .style("background", "#1f1f1fff")
+        .style("color", "#ffffffff")
+        .style("font-size", "14px")
+        .text(d => d.label)
+
+        //handiling of clicking button
+        .on("click", function(event, d) {
+
+          d3.select("#visualization-2").select("svg").remove();
+          d3.select(this).style("background", "#307c32ff");
+          buttons.style("background", "#1f1f1fff"); 
+
+          calendarMap = new CalendarMap("visualization-2", d);
+          calendarMap.loadFlights();
+
+        });     
+      
+}
+
+
+
